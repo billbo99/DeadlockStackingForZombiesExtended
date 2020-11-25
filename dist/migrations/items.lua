6673e318f60e8e09ed0b140399d3ea39041fdcae
billbo99 <cactus_computers-github@yahoo.com>
2020-11-25 08:24:16 +0000

    first commit

---
+++ dist/migrations/items.lua
@@ -0,0 +1,27 @@
+local Items = {}
+
+Items.items = {
+    ["beacon-mk1"] = {tech = "effect-transmission-mk1"},
+    ["beacon-mk2"] = {tech = "effect-transmission-mk2"},
+    ["beacon-mk3"] = {tech = "effect-transmission-mk3"},
+    ["wall-mk1"] = {tech = "vibranium-walls-mk1"},
+    ["wall-mk2"] = {tech = "vibranium-walls-mk2"},
+    ["wall-mk3"] = {tech = "vibranium-walls-mk3"},
+    ["speed-module-mk1"] = {type = "module", tech = "speed-module-mk1"},
+    ["speed-module-mk2"] = {type = "module", tech = "speed-module-mk2"},
+    ["speed-module-mk3"] = {type = "module", tech = "speed-module-mk3"},
+    ["effectivity-module-mk1"] = {type = "module", tech = "effectivity-module-mk1"},
+    ["effectivity-module-mk2"] = {type = "module", tech = "effectivity-module-mk2"},
+    ["effectivity-module-mk3"] = {type = "module", tech = "effectivity-module-mk3"},
+    ["productivity-module-mk1"] = {type = "module", tech = "productivity-module-mk1"},
+    ["productivity-module-mk2"] = {type = "module", tech = "productivity-module-mk2"},
+    ["productivity-module-mk3"] = {type = "module", tech = "productivity-module-mk3"},
+    ["sniper-ammo-mk1"] = {type = "ammo", tech = "advanced-rifles-mk1"},
+    ["sniper-ammo-mk2"] = {type = "ammo", tech = "advanced-rifles-mk2"},
+    ["sniper-ammo-mk3"] = {type = "ammo", tech = "advanced-ammo-mk3"},
+    ["assault-ammo-mk1"] = {type = "ammo", tech = "advanced-rifles-mk1"},
+    ["assault-ammo-mk2"] = {type = "ammo", tech = "advanced-rifles-mk1"},
+    ["assault-ammo-mk3"] = {type = "ammo", tech = "advanced-ammo-mk3"}
+}
+
+return Items
